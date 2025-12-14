f929e4edb6e3627ba9e1878d00007996ea840a7b
LogNesquik <logenerlogenerovic@gmail.com>
2025-12-14 17:27:44 +0300

    Выполнил ДЗ

---
+++ homework_5.py
@@ -0,0 +1,23 @@
+class Vehicle:
+    def start(self):
+        print("vehicle started")
+
+
+class Car(Vehicle):
+    def start(self):
+        super().start()
+        print("car started")
+
+class ElectricCar(Vehicle):
+    def start(self):
+        super().start()
+
+
+class Tesla(ElectricCar, Car):
+    def start(self):
+        super().start()
+        print("tesla ready")
+
+tesla = Tesla()
+tesla.start()
+print(Tesla.mro())
