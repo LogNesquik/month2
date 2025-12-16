89e90b0c07d07a456210c16f8b2600c34c7ed323
LogNesquik <logenerlogenerovic@gmail.com>
2025-12-16 15:53:41 +0300

    Выполнил классую работу за 6 урок

---
+++ lessons/lesson_6.py
@@ -0,0 +1,7 @@
+# lessons - lesson_1.py
+from lessons.lesson_1 import Car
+# или же можно from lesson_1 import Car, 
+# Работает только при условии что файл внутри папки.
+
+car_honda = Car(color = 'blue', model = 'honda')
+
