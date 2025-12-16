0a1a1eae7096d3c9ac65bb0c7cfa66a0799c5845
LogNesquik <logenerlogenerovic@gmail.com>
2025-12-16 13:54:46 +0300

    Изменил lesson_5, и удалил один файл - classwork#5.py

---
+++ lesson_5.py
@@ -0,0 +1,19 @@
+class Animal:
+    def move(self):
+        print("двигается")
+
+class Swimming(Animal):
+    def move(self):
+        print("плавает")
+
+class Flying(Animal):
+    def move(self):
+        print("летает")
+
+
+class Duck(Swimming, Flying):
+    pass
+
+
+duck = Duck()
+duck.move()
