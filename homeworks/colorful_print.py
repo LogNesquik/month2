f776c3aa73a1ec98581f0ad2693bf3e42cf42c68
LogNesquik <logenerlogenerovic@gmail.com>
2025-12-17 11:33:51 +0300

    Переиминовал файл, и добавил импорт, экземпляры класса Person.

--- homeworks/colorful_print.py
+++ homeworks/colorful_print.py
@@ -1,4 +1,5 @@
 from blessed import Terminal
+from homework_1 import Person
 
 term = Terminal()
 
@@ -15,3 +16,11 @@ fruct_color = {
 print(term.bold_underline('Fruit Color Display'))
 for fruit, color in fruct_color.items():
     print(color(fruit))
+
+
+people = Person('Иван', '01.01.90', 'Инженер', True)
+people_2 = Person('Мария', '15.05.85', 'Врач', False)
+people_3 = Person('Алексей', '20.10.92', 'Учитель', True)
+people.introduce()
+people_2.introduce()
+people_3.introduce()
